605405a5447ca20996ce2972313bb5b8ecfc4a00
Anton Vorobyov <myredsocks@inbox.ru>
2016-04-21 19:40:46 +0600

    second commit

--- TestCatalog/Content/js/app/controller/users-controller.js
+++ TestCatalog/Content/js/app/controller/users-controller.js
@@ -6,14 +6,26 @@
         .controller("usersController", [
             "$scope", "userService", function ($scope, userService) {
 
-                userService
-                    .users()
-                    .success(function (response) {
-                        $scope.users = response.items;
-                        $scope.total = response.total;
-                    });
+                var toPaging = function(pages) {
+                    var array = [];
+                    for (var i = 1; i <= pages; i++) {
+                        array.push(i);
+                    }
+                    return array;
+                };
 
-                $scope.title = "yooo";
+                $scope.getUsers = function (page) {
+                    userService
+                        .users({ page: page || 1 })
+                        .success(function(response) {
+                            $scope.users = response.items;
+                            $scope.total = response.total;
+                            $scope.pages = toPaging(response.pages);
+                            $scope.page = response.page;
+                        });
+                }
+
+                $scope.getUsers();
             }
         ]);
 })();
